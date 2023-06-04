2215057270cff62e401d8116df38bb5cdd625cc8
Jim Lloyd <jim@play-hearts.online>
2023-06-04 16:59:16 -0700

    gstate_wasm_test: api.spec.ts only

--- packages/gstate_wasm_test/src/index.ts
+++
@@ -1,9 +0,0 @@
-import factory from "@playhearts/gstate_wasm";
-
-import type { GStateModule } from "@playhearts/gstate_wasm";
-
-export const instance: GStateModule = await factory();
-
-console.log(instance);
-
-
