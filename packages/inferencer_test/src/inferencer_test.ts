1958c926da4fbd8c60b80a22259dfa89360fe8e8
Jim Lloyd <jim@play-hearts.online>
2023-08-05 22:40:55 -0700

    Use both pass and play standard models

--- packages/inferencer_test/src/inferencer_test.ts
+++ packages/inferencer_test/src/inferencer_test.ts
@@ -1,8 +1,9 @@
 import factory, { Trick, TrickOrdRep } from '@playhearts/gstate_wasm'
-import { type FloatArraySpec, newFloatArray, asMin2022InputTensor } from '@playhearts/cards_ts'
-import { createConnection, type Socket } from "node:net"
+import { type FloatArraySpec, newFloatArray, asMin2022InputTensor, cardSetAsOrds, freeFloatArray } from '@playhearts/cards_ts'
+import Socket from "./Socket.js"
 import type { GState } from '@playhearts/gstate_wasm'
 import { Buffer } from 'node:buffer'
+import { EventEmitter } from 'node:events'
 
 import type * as gstate_wasm from '@playhearts/gstate_wasm'
 
@@ -11,27 +12,50 @@ import chaiAsPromised from 'chai-as-promised'
 chai.use(chaiAsPromised)
 const { expect } = chai
 
-async function receiveData(session: Socket, size: number): Promise<Float32Array> {
-    const expectedBytes: number = size * Float32Array.BYTES_PER_ELEMENT
-    return new Promise((resolve, reject) => {
-        let partial: Buffer = null
-        session.on('data', (chunk: Buffer) => {
-            partial = partial == null ? chunk : Buffer.concat([partial, chunk])
-            if (partial.length > expectedBytes) {
-                reject(new Error("got more data than expected!"))
-            } else if (partial.length == expectedBytes) {
-                const result: Float32Array = new Float32Array(partial.buffer, partial.byteOffset, partial.byteLength / Float32Array.BYTES_PER_ELEMENT)
-                expect(result.BYTES_PER_ELEMENT).to.equal(4)
-                expect(result.length).to.equal(52)
-                expect(result.byteLength).to.equal(expectedBytes)
-                resolve(result)
-            }
-        })
-        session.on('error', reject)
-    })
+function createSocket(): Socket {
+    return new Socket()
 }
 
-export async function runInferrence(session: Socket, gstate: GState, spec: FloatArraySpec): Promise<Float32Array> {
+async function receiveData(playSession: Socket, size: number): Promise<Float32Array> {
+    const expectedBytes: number = size * Float32Array.BYTES_PER_ELEMENT
+    let buffer: Buffer = Buffer.alloc(0)
+    while (true) {
+        const needed = expectedBytes - buffer.length
+        const chunk: Buffer = Buffer.from(await playSession.read(needed))
+        buffer = buffer == null ? chunk : Buffer.concat([buffer, chunk])
+        if (buffer.length == expectedBytes) break
+        else if (buffer.length > expectedBytes) throw new Error("got more data than expected!")
+    }
+    const result: Float32Array = new Float32Array(buffer.buffer, buffer.byteOffset, buffer.byteLength / Float32Array.BYTES_PER_ELEMENT)
+    expect(result.BYTES_PER_ELEMENT).to.equal(4)
+    expect(result.length).to.equal(size)
+    expect(result.byteLength).to.equal(expectedBytes)
+    return result
+}
+
+export async function runBidInferrence(bidSession: Socket, spec: FloatArraySpec): Promise<Float32Array> {
+    try {
+        expect(spec.arr.BYTES_PER_ELEMENT).to.equal(4)
+        const expectedSize = 52 * 1 * Float32Array.BYTES_PER_ELEMENT
+        expect(spec.arr.byteLength).to.equal(expectedSize)
+        const buffer: Buffer = Buffer.copyBytesFrom(spec.arr)
+        if (buffer.byteLength != expectedSize) throw new Error(`Buffer size ${buffer.byteLength} is not expected size ${expectedSize}`)
+        await bidSession.write(buffer)
+
+        const outputElements: number = 52
+        const data: Float32Array = await receiveData(bidSession, outputElements)
+        expect(data.BYTES_PER_ELEMENT).to.equal(4)
+        expect(data.length).to.equal(52)
+        expect(data.byteLength).to.equal(52 * 4)
+        return data
+    }
+    catch (err) {
+        console.error("Inference failed:", err.message)
+        throw err
+    }
+}
+
+export async function runInferrence(playSession: Socket, gstate: GState, spec: FloatArraySpec): Promise<Float32Array> {
 
     asMin2022InputTensor(spec, gstate)
     try {
@@ -40,10 +64,10 @@ export async function runInferrence(session: Socket, gstate: GState, spec: Float
         expect(spec.arr.byteLength).to.equal(expectedSize)
         const buffer: Buffer = Buffer.copyBytesFrom(spec.arr)
         if (buffer.byteLength != expectedSize) throw new Error(`Buffer size ${buffer.byteLength} is not expected size ${expectedSize}`)
-        session.write(buffer)
+        playSession.write(buffer)
 
         const outputElements: number = 52
-        const data: Float32Array = await receiveData(session, outputElements)
+        const data: Float32Array = await receiveData(playSession, outputElements)
         expect(data.BYTES_PER_ELEMENT).to.equal(4)
         expect(data.length).to.equal(52)
         expect(data.byteLength).to.equal(52 * 4)
@@ -57,35 +81,64 @@ export async function runInferrence(session: Socket, gstate: GState, spec: Float
 
 
 async function createSession(modelPath: string): Promise<Socket> {
-    return new Promise((resolve, reject) => {
-        const session: Socket = createConnection(51515, "ryley.lan", async () => {
-            session.on('data', (chunk: Buffer) => {
-                const response = chunk.toString()
-                if (response.length == 2 && response[0] == 'O' && response[1] == 'K') {
-                    resolve(session)
-                }
-            })
-            const modelSpec = `{ "model": "${modelPath}" }`
-            console.log("Sending model spec:", modelSpec)
-            session.write(modelSpec)
-        })
-    })
+    const session = createSocket()
+    await session.connect(51515, "ryley.lan")
+    const modelSpec = `{ "model": "${modelPath}" }`
+    console.log("Sending model spec:", modelSpec)
+    await session.write(Buffer.from(modelSpec))
+    const chunk = await session.read(2)
+    const response = chunk.toString()
+    if (response.length == 2 && response[0] == 'O' && response[1] == 'K') {
+        return session
+    }
+    throw new Error("Failed to create session")
+}
+
+interface ChooseThreeArgs {
+    instance: gstate_wasm.GStateModule,
+    gstate: gstate_wasm.GState,
+    bidSession: Socket,
+    player: number,
+    bidSpec: FloatArraySpec,
+}
+
+async function chooseThree(args: ChooseThreeArgs): Promise<void> {
+    const { instance, gstate, bidSession, player, bidSpec } = args
+    const _hand: gstate_wasm.CardSet = gstate.playersHand(player)
+    const hand: number[] = cardSetAsOrds(_hand)
+
+    bidSpec.arr.fill(0.0)
+    hand.forEach((card: number): void => { bidSpec.arr[card] = 1.0 })
+
+    const outputData: Float32Array = await runBidInferrence(bidSession, bidSpec)
+    for (let c = 0; c < 52; c++) {
+        if (!hand.includes(c)) outputData[c] = 999.0
+    }
+    const result: number[] = Array.from(outputData.keys())
+        .sort((a: number, b: number): number => outputData[a] - outputData[b])
+        .slice(0, 3)
+
+    const pass: gstate_wasm.CardSet = new instance.CardSet()
+    result.forEach((card: number): void => { pass.addOrd(card) })
+    gstate.setPassFor(player, pass)
+    pass.delete()
 }
 
 export async function playOutGame(instance: gstate_wasm.GStateModule, gstate: gstate_wasm.GState): Promise<void> {
     const passOffset = gstate.passOffset()
     if (passOffset > 0) {
-        for (let i = 0; i < 4; i++) {
-            let hand = gstate.playersHand(i)
-            let pass = instance.chooseThreeAtRandom(hand)
-            gstate.setPassFor(i, pass)
+        const bidSession: Socket = await createSession("/opt/pho/models/standard/bid/e112.pt")
+        console.log("Pass offset:", passOffset)
+        const bidSpec: FloatArraySpec = newFloatArray(instance, 52)
+        for (let player = 0; player < 4; player++) {
+            await chooseThree({ instance, gstate, bidSession, player, bidSpec })
         }
+        freeFloatArray(instance, bidSpec)
     }
     gstate.startGame()
 
-    const modelPath = "/opt/pho/models/standard/play/e165.pt"
-    const session: Socket = await createSession(modelPath)
-    const spec: FloatArraySpec = newFloatArray(instance, 52 * 12)
+    const playSession: Socket = await createSession("/opt/pho/models/standard/play/e165.pt")
+    const playSpec: FloatArraySpec = newFloatArray(instance, 52 * 12)
 
     while (!gstate.done()) {
         const p: number = gstate.currentPlayer()
@@ -103,7 +156,7 @@ export async function playOutGame(instance: gstate_wasm.GStateModule, gstate: gs
             continue
         }
 
-        const outputData: Float32Array = await runInferrence(session, gstate, spec)
+        const outputData: Float32Array = await runInferrence(playSession, gstate, playSpec)
         expect(outputData).to.exist
         expect(outputData.BYTES_PER_ELEMENT).to.equal(4)
         expect(outputData.length).to.equal(52)
@@ -147,6 +200,8 @@ export async function playOutGame(instance: gstate_wasm.GStateModule, gstate: gs
         prior.delete()
     }
 
+    freeFloatArray(instance, playSpec)
+
     const playerScores: gstate_wasm.PlayerScores = gstate.getPlayerScores()
 
     for (let p = 0; p < 4; ++p) {
@@ -159,6 +214,7 @@ export async function playOutGame(instance: gstate_wasm.GStateModule, gstate: gs
 }
 
 async function main(): Promise<void> {
+    EventEmitter.setMaxListeners(0) // default is 10, which is too low for us
     const instance = await factory()
 
     const init: gstate_wasm.GStateInit = instance.kRandomVal()
