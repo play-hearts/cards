eb670c32481ae4edc4d1a8e62bae36b91c73e56a
Jim Lloyd <jim@play-hearts.online>
2023-08-06 17:47:12 -0700

    Socket: tighter code

--- packages/inferencer/src/Socket.ts
+++ packages/inferencer/src/Socket.ts
@@ -1,5 +1,8 @@
 import net from 'node:net'
 import debug from 'debug'
+import { once } from 'node:events'
+import { setTimeout } from 'node:timers/promises'
+import util from 'node:util'
 
 const dlog = debug('Socket')
 
@@ -11,80 +14,62 @@ export default class Socket {
     }
 
     public async connect(port: number, host: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this._socket.on('connect', () => {
-                dlog(`Connected to ${host}:${port}`)
-                resolve()
-            })
-            this._socket.on('error', (err) => {
-                dlog(`Error connecting to ${host}:${port}: ${err.message}`)
-                reject(err)
-            })
-            dlog(`Connecting to ${host}:${port}`)
-            this._socket.connect(port, host)
-        })
+        const connectPromise = once(this._socket, 'connect')
+        this._socket.connect(port, host)
+        try {
+            await connectPromise
+            dlog(`Connected to ${host}:${port}`)
+        }
+        catch (err) {
+            dlog(`Error connecting to ${host}:${port}: ${err.message}`)
+            throw err
+        }
     }
 
     public async connectUnix(path: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this._socket.on('connect', () => {
-                dlog(`Connected to ${path}`)
-                resolve()
-            })
-            this._socket.on('error', (err) => {
-                dlog(`Error connecting to ${path}: ${err.message}`)
-                reject(err)
-            })
-            dlog(`Connecting to ${path}`)
-            this._socket.connect(path)
-        })
+        const connectPromise = once(this._socket, 'connect')
+        this._socket.connect(path)
+        try {
+            await connectPromise
+            dlog(`Connected to ${path}`)
+        }
+        catch (err) {
+            dlog(`Error connecting to ${path}: ${err.message}`)
+            throw err
+        }
     }
 
-    public async read(size: number, timeoutMs: number = 5000): Promise<Buffer> {
-        return new Promise<Buffer>((resolve, reject) => {
-            const t = setTimeout(() => {
-                dlog(`Timeout reading ${size} bytes`)
-                reject(new Error(`Timeout reading ${size} bytes`))
-            }, timeoutMs)
-            this._socket.on('error', (err) => {
-                dlog(`Error reading ${size} bytes: ${err.message}`)
-                reject(err)
-            })
-            this._socket.once('data', (data) => {
-                clearTimeout(t)
-                dlog(`Read ${data.byteLength} of ${size} bytes`)
-                resolve(data)
-            })
-            dlog(`Reading ${size} bytes`)
-            this._socket.read(size)
-        })
+    public async read(size: number, timeoutMs: number = 20000): Promise<Buffer> {
+        const dataPromise = once(this._socket, 'data')
+        const timeoutPromise = setTimeout(timeoutMs, new Error("Timeout"))
+        this._socket.read(size)
+        const result = await Promise.race([dataPromise, timeoutPromise])
+        if (result instanceof Error) throw result
+        const [chunk] = result as [Buffer]
+        return chunk
     }
 
     public async write(buffer: Buffer): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this._socket.on('error', (err) => {
-                dlog(`Error writing ${buffer.byteLength} bytes: ${err.message}`)
-                reject(err)
-            })
-            dlog(`Writing ${buffer.byteLength} bytes`)
-            this._socket.write(buffer, () => {
-                dlog(`Wrote ${buffer.byteLength} bytes`)
-                resolve()
-            })
-        })
+        const write = util.promisify(this._socket.write).bind(this._socket)
+        const timeoutPromise = setTimeout(100, new Error("Write Timeout"))
+        const writePromise = write(buffer)
+        const result = await Promise.race([writePromise, timeoutPromise])
+        if (result instanceof Error) throw result
     }
 
     public async end(): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this._socket.on('error', (err) => {
-                dlog(`Error ending socket: ${err.message}`)
-                reject(err)
-            })
-            dlog(`Ending socket`)
-            this._socket.end(() => {
-                dlog(`Ended socket`)
-                resolve()
-            })
-        })
+        const closePromise = once(this._socket, 'close')
+
+        dlog(`Closing socket`)
+        this._socket.end()
+
+        try {
+            await closePromise
+            dlog(`Socket closed`)
+        }
+        catch (err) {
+            dlog(`Error closing socket: ${err.message}`)
+            throw err
+        }
     }
 }
